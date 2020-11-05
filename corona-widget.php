b3534134dfd2461231357a7f93da22bc634bec63
Jan Uruba <jan@uruba.eu>
2020-11-05 15:58:05 +0100

    Plugin init

---
+++ corona-widget.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Corona Widget
+ * Plugin URI:
+ * Description: Jednoduchý WP widget s denní statistikou výskytu korony v ČR.
+ * Version: 0.0.1
+ * Author: Jan Uruba
+ * Author URI: https://uruba.eu
+ * Licence: MIT
+ *
+ * Text Domain: corona
+ *
+ * */
+
+defined( 'ABSPATH' ) || exit;
